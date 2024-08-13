1552ea6ff045057c3b429987cd46c33667ab72c3
github_username_here <email@example.com>
2024-08-13 13:26:09 +1000

    2: Add feature enter guest numbers and error message

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Enter Guests Name
 def get_guest_name():
     while True:
         guest_name = input("Enter Guests Name: ")
@@ -9,4 +10,14 @@ def get_guest_name():
 guest_name = get_guest_name()
 print(f"Welcome, {guest_name}!")
 
-number_of_guests = input("Number of Guests: ")
+# Enter number of guests
+def get_number_of_guests():
+    while True:
+        guest_number = input("Enter Number of Guests: ")
+        if guest_number.isdigit():
+                return guest_number
+        else:
+            print("Error: Number should only contain numeric values. Please try again.")
+
+guest_number = get_number_of_guests()  # Get the numeric value
+
