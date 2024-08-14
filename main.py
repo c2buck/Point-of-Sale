da1736a28f959ac3104b065cc1818e373074b90f
github_username_here <email@example.com>
2024-08-14 21:05:30 +1000

    3. Add feature request apartment ID

--- main.py
+++ main.py
@@ -15,9 +15,21 @@ def get_number_of_guests():
     while True:
         guest_number = input("Enter Number of Guests: ")
         if guest_number.isdigit():
-                return guest_number
+            return guest_number
         else:
             print("Error: Number should only contain numeric values. Please try again.")
 
 guest_number = get_number_of_guests()  # Get the numeric value
+
+#enter apartment ID
+
+def get_apartment_ID():
+    while True:
+        apartment_ID = input("Enter the apartment ID, example Unit 12 Swan building = U12swan: ")
+        if apartment_ID.startswith("U"):
+            return apartment_ID
+        else:
+             print ("Error: Apartment ID must start with U for Unit")
+                    
+apartment_ID = get_apartment_ID()   
 
